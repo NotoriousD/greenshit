6e2184e86cad89a2fb4bd9ea42d35aaabd1f89f8
Danil <test@dsad>
2016-06-16 01:31:45 +0300

    1

--- app/js/common.js
+++ app/js/common.js
@@ -1,50 +1,60 @@
 $(function() {
-    
-        var owl = $(".carousel");
-	owl.owlCarousel({
-		items : 1,
-        autoHeight : true
-	});
-	owl.on("mousewheel", ".owl-wrapper", function (e) {
-		if (e.deltaY > 0) {
-			owl.trigger("owl.prev");
-		} else {
-			owl.trigger("owl.next");
-		}
-		e.preventDefaults();
-	});
-	$(".next_button").click(function(){
-		owl.trigger("owl.next");
-	});
-	$(".prev_button").click(function(){
-		owl.trigger("owl.prev");
-	});
 
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
+    var owl = $(".carousel");
+    owl.owlCarousel({
+        items : 1,
+        autoHeight : true,
+        singleItem:true
+    });
+    owl.on("mousewheel", ".owl-wrapper", function (e) {
+        if (e.deltaY > 0) {
+            owl.trigger("owl.prev");
+        } else {
+            owl.trigger("owl.next");
+        }
+        e.preventDefaults();
+    });
+    $(".next_button").click(function(){
+        owl.trigger("owl.next");
+    });
+    $(".prev_button").click(function(){
+        owl.trigger("owl.prev");
+    });
 
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-    
+    //SVG Fallback
+    if(!Modernizr.svg) {
+        $("img[src*='svg']").attr("src", function() {
+            return $(this).attr("src").replace(".svg", ".png");
+        });
+    };
+
+    //E-mail Ajax Send
+    //Documentation & Example: https://github.com/agragregra/uniMail
+    $("form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+    $(".top_menu_button").on("click",function(){
+        $(this).toggleClass('active_menu');
+        $(this).find("i").toggleClass('fa-bars fa-times');
+        $("#nav").toggleClass("active_menu");
+    });
+    $(".middle_menu_button").on("click",function(){
+        $(this).toggleClass('active_menu');
+        $(this).find("i").toggleClass('fa-bars fa-times');
+        $("#middle__nav").toggleClass("active_menu");
+    });
 });
 
 $(document).ready(function(){
